e09545815898416ce1c926c69de95a72a843137c
tidjee-dev <pinet.donatien@gmail.com>
2025-07-30 23:26:28 +0200

    👌 IMPROVE: update mysql-init.EXAMPLE.sql

--- .docker/mysql/mysql-init.EXAMPLE.sql
+++ .docker/mysql/mysql-init.EXAMPLE.sql
@@ -1,3 +1,15 @@
+-- Create a new MySQL user with the username 'MYSQL_USER' that can connect from any host
 CREATE USER IF NOT EXISTS 'MYSQL_USER'@'%' IDENTIFIED BY 'MYSQL_PASSWORD';
+
+-- Grant all privileges to 'MYSQL_USER' for all databases and tables, allowing connections from any host
 GRANT ALL PRIVILEGES ON *.* TO 'MYSQL_USER'@'%';
+
+-- Create a new MySQL user with the username 'MYSQL_USER' that can connect from localhost
+CREATE USER IF NOT EXISTS 'MYSQL_USER'@'localhost' IDENTIFIED BY 'MYSQL_PASSWORD';
+
+-- Grant all privileges to 'MYSQL_USER' for all databases and tables, allowing connections from localhost
+GRANT ALL PRIVILEGES ON *.* TO 'MYSQL_USER'@'localhost';
+
+-- Reload the grant tables in the MySQL database to ensure that privilege changes take effect immediately
 FLUSH PRIVILEGES;
+
